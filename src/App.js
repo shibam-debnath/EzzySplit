a82e96146c609191d48a6ee3c118dcba90d93ce4
noob_coder <shibamdebnath07@gmail.com>
2022-12-11 14:59:06 +0530

    directory set-up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import DashBoard from "./pages/DashBoard/DashBoard";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/dashBoard" element={<DashBoard />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Feature from "../../components/Feature/Feature";
+import Hero from "../../components/Hero/Hero";
+
+const Home = () => {
+  return (
+    <div>
+      <Hero />
+      <Feature />
+    </div>
+  );
+};
+
+export default Home;
