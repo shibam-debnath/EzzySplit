9df7c9022846dac792a7470ca226429912b40bbd
Mohit <mohitranjan2001r@gmail.com>
2022-12-13 21:12:40 +0530

    footer added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,94 +2,107 @@ import React from "react";
 import ReactDOM from "react-dom";
 const Footer = () => {
    return (
-    <footer class="bg-white dark:bg-gray-800">
-<div class="max-w-screen-xl p-4 py-6 mx-auto lg:py-16 md:p-8 lg:p-10">
-<div class="grid grid-cols-2 gap-8 md:grid-cols-3 lg:grid-cols-5">
+    <footer className="bg-gray-900">
+<div className="w-full">
+<div className="grid grid-cols-2 gap-8 md:grid-cols-2 lg:grid-cols-4 text-center">
 <div>
-<h3 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Company</h3>
-<ul class="text-gray-500 dark:text-gray-400">
-<li class="mb-4">
-<a href="#" class=" hover:underline">About</a>
+<h3 className="mb-6  font-semibold  uppercase text-white">Company</h3>
+<ul className="text-gray-500 dark:text-gray-400">
+<li className="mb-2">
+<a href="#" className=" hover:underline">About</a>
 </li>
-<li class="mb-4">
-<a href="#" class="hover:underline">Careers</a>
+<li className="mb-2">
+<a href="#" className="hover:underline">Careers</a>
 </li>
-<li class="mb-4">
-<a href="#" class="hover:underline">Brand Center</a>
+<li className="mb-2">
+<a href="#" className="hover:underline">Brand Center</a>
 </li>
-<li class="mb-4">
-<a href="#" class="hover:underline">Blog</a>
+<li className="mb-2">
+<a href="#" className="hover:underline">Blog</a>
 </li>
 </ul>
 </div>
 <div>
-<h3 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Help center</h3>
-<ul class="text-gray-500 dark:text-gray-400">
-<li class="mb-4">
-<a href="#" class="hover:underline">Discord Server</a>
+<h3 className="mb-6  font-semibold text-white uppercase ">Help center</h3>
+<ul className="text-gray-500 dark:text-gray-400">
+<li className="mb-2">
+<a href="#" className="hover:underline">Discord Server</a>
 </li>
-<li class="mb-4">
-<a href="#" class="hover:underline">Twitter</a>
+<li className="mb-2">
+<a href="#" className="hover:underline">Twitter</a>
 </li>
-<li class="mb-4">
-<a href="#" class="hover:underline">Facebook
-</a></li><a href="#" class="hover:underline">
-</a><li class="mb-4"><a href="#" class="hover:underline">
-</a><a href="#" class="hover:underline">Contact Us</a>
-</li>
-</ul>
-</div>
-<div>
-<h3 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h3>
-<ul class="text-gray-500 dark:text-gray-400">
-<li class="mb-4">
-<a href="#" class="hover:underline">Privacy Policy</a>
-</li>
-<li class="mb-4">
-<a href="#" class="hover:underline">Licensing</a>
-</li>
-<li class="mb-4">
-<a href="#" class="hover:underline">Terms</a>
+<li className="mb-2">
+<a href="#" className="hover:underline">Facebook
+</a></li><a href="#" className="hover:underline">
+</a><li className="mb-3"><a href="#" className="hover:underline">
+</a><a href="#" className="hover:underline">Contact Us</a>
 </li>
 </ul>
 </div>
 <div>
-<h3 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Company</h3>
-<ul class="text-gray-500 dark:text-gray-400">
-<li class="mb-4">
-<a href="#" class=" hover:underline">About</a>
+<h3 className="mb-6  font-semibold text-white uppercase ">Legal</h3>
+<ul className="text-gray-500 dark:text-gray-400">
+<li className="mb-2">
+<a href="#" className="hover:underline">Privacy Policy</a>
 </li>
-<li class="mb-4">
-<a href="#" class="hover:underline">Careers</a>
-</li>
-<li class="mb-4">
-<a href="#" class="hover:underline">Brand Center</a>
-</li>
-<li class="mb-4">
-<a href="#" class="hover:underline">Blog</a>
-</li>
-</ul>
-</div>
-<div>
-<h3 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Download</h3>
-<ul class="text-gray-500 dark:text-gray-400">
-<li class="mb-4">
-<a href="#" class="hover:underline">iOS</a>
-</li>
-<li class="mb-4">
-<a href="#" class="hover:underline">Android</a>
-</li>
-<li class="mb-4">
-<a href="#" class="hover:underline">Windows</a>
-</li>
-<li class="mb-4">
-<a href="#" class="hover:underline">MacOS</a>
-</li>
-</ul>
-</div>
-</div>
 
+<li className="mb-2">
+<a href="#" className="hover:underline">Terms</a>
+</li>
+</ul>
+</div>
+<div>
 
+<h3 className="mb-6 font-semibold text-white uppercase ">Use Case</h3>
+<ul className="text-gray-500 dark:text-gray-400">
+<li className="mb-2">
+<a href="#" className="hover:underline">Holidays & weekends</a>
+</li>
+<li className="mb-2">
+<a href="#" className="hover:underline">Shared house</a>
+</li>
+<li className="mb-2">
+<a href="#" className="hover:underline">Bachelor party</a>
+</li>
+<li className="mb-2">
+<a href="#" className="hover:underline">other</a>
+</li>
+</ul>
+</div>
+</div>
+<div className="text-center  mt-10">
+<a href="#" className="flex items-center justify-center mb-5 text-2xl font-semibold  text-gray-500">
+<img src="./images/logo.jpg" className="h-6 mr-3 sm:h-9" alt="EzzySplit Logo"/>
+EzzySplit
+</a>
+<span className="block text-sm text-center text-gray-500 dark:text-gray-400">© 2022 EzzySplit™. All Rights Reserved.
+</span>
+
+</div>
+<div className ="mt-4">
+   <ul className="flex justify-center space-x-5">
+   <li className="hover:scale-[1.5]">
+      <a href="#" >
+         <img src="./images/fb_logo.png" alt=" " className="h-8 "></img>
+      </a>
+      </li>
+      <li className="hover:scale-[1.5]">
+      <a href="#" >
+         <img src="./images/twitt_logo.png" alt=" " className="h-8"></img>
+      </a>
+      </li>
+      <li className="hover:scale-[1.5]">
+      <a href="#" >
+         <img src="./images/insta_logo.png" alt=" " className="h-8 "></img>
+      </a>
+      </li>
+      <li className="hover:scale-[1.5]">
+      <a href="#" >
+         <img src="./images/git_logo.png" alt=" " className="h-8 "></img>
+      </a>
+      </li>
+   </ul>
+</div>
 </div>
 </footer>
    )
