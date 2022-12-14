21ec084e871c7afad175467c1ebc17b860fc163e
0024nv <nikhilvinay24@gmail.com>
2022-12-14 22:53:33 +0530

    Dashborad schema created

---
+++ src/pages/DashBoard/DashComp/LeftSection/Left.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Left = ()=>{
+    return<>
+          <p>
+        <span className="pl-5 pr-6">Left(lg)</span>
+          Dashboard
+          </p>
+          <p>
+        <span className="pl-5 pr-6">Left(lg)</span>
+        Transactions
+          </p>
+    </>
+}
+export default Left;
