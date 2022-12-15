37f0187d690189275fb713f1b2765ed77650e5f0
noob_coder <shibamdebnath07@gmail.com>
2022-12-15 18:23:23 +0530

    react-icon added

--- src/pages/DashBoard/DashComp/MidSection/Mid.jsx
+++ src/pages/DashBoard/DashComp/MidSection/Mid.jsx
@@ -1,15 +1,59 @@
 import React from "react";
-
+import Button from "../../../../components/Button/Button";
+import { earningData } from "../../data";
 const Mid = () => {
-    return <>
-        <p>
-            mid Section
-        </p>
-       
-       <p>
-        Lorem ipsum, Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste, quisquam neque. Ex minima sint amet repellat tempore corrupti facilis aliquam provident animi assumenda sit ipsa alias quasi facere, error quod. dolor sit amet consectetur adipisicing elit. Sit soluta temporibus reiciendis deserunt incidunt perferendis esse et architecto facere corporis!
-       </p>
-    </>
-}
+  return (
+    <div className="mt-0">
+      <div className="flex flex-wrap justify-center ">
+        <div className="bg-white w-full dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl   p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
+          <div className="flex justify-between items-center">
+            <div>
+              <p className="font-bold text-gray-400 align-left">Hi</p>
+              <p className="text-2xl">shibam</p>
+            </div>
+            {/* <button
+              type="button"
+              style={{ backgroundColor: currentColor }}
+              className="text-2xl opacity-0.9 text-white hover:drop-shadow-xl rounded-full  p-4"
+            >
+              <BsCurrencyDollar />
+            </button> */}
+          </div>
+          <div className="mt-6">
+            <Button
+              color="white"
+              bgColor="white"
+              text="Download"
+              borderRadius="10px"
+            />
+          </div>
+        </div>
+        <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
+          {earningData.map((item) => (
+            <div
+              key={item.title}
+              className="bg-white h-44 dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56  p-4 pt-9 rounded-2xl "
+            >
+              <button
+                type="button"
+                style={{ color: item.iconColor, backgroundColor: item.iconBg }}
+                className="text-2xl opacity-0.9 rounded-full  p-4 hover:drop-shadow-xl"
+              >
+                {item.icon}
+              </button>
+              <p className="mt-3">
+                <span className="text-lg font-semibold">{item.amount}</span>
+                <span className={`text-sm text-${item.pcColor} ml-2`}>
+                  {item.percentage}
+                </span>
+              </p>
+              <p className="text-sm text-gray-400  mt-1">{item.title}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Mid;
