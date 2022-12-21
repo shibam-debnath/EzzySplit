ca2172ece01ff61682dbbcc6a8f943df136b70b1
Aniket Bose <97694136+Ani-Bos@users.noreply.github.com>
2022-12-21 22:29:46 +0530

    Login authentication done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,22 @@ import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 import DashBoard from "./pages/DashBoard/DashBoard";
+import Login from "./pages/Login/Login";
+
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import initializeAuthentication from "./firebase/firebase.init";
+initializeAuthentication();
+const provider = new GoogleAuthProvider();
+
+const handleGooglesignIn = () => {
+  const auth = getAuth();
+
+  signInWithPopup(auth, provider).then((result) => {
+    const user = result.user;
+    console.log(user);
+  });
+};
+
 
 function App() {
   return (
@@ -12,6 +28,10 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/dashBoard" element={<DashBoard />} />
+        <Route
+          path="/Login"
+          element={<Login handleGooglesignIn={handleGooglesignIn} />}
+        />
       </Routes>
       <Footer />
     </div>
