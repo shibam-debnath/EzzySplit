26cf8a927b32cf5d390335c0997e5a16ce28aab9
Suraj Kumar <sraj15082003@gmail.com>
2023-01-30 16:26:26 +0530

    Backend with sending mail of contact form added

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,111 +1,178 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import axios from "axios";
 
 const Contact = () => {
-    //handle events
-    const { register, handleSubmit, watch, formState: { errors } } = useForm({
-        mode:'onTouched'
-    });
+  //handle events
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    mode: "onTouched",
+  });
 
-    //handle submit
-    const onSubmit = data => alert(JSON.stringify(data));
+  //handle submit
+  const url = "http://localhost:8000/sendmail/contactUs";
+  async function onSubmitForm(values) {
+    console.log(values);
 
-    return (
-        <div className="w-full bg-transparent p-6 text-white">
-            <h1 className="font-bold text-center text-2xl sm:text-3xl"><span className="text-primary">Contact</span> Us</h1>
+    let config = {
+      method: "post",
+      url: "http://localhost:8000/sendmail/contactUs",
+      // headers:{
+      //     'Content-type':'applcation/json'
+      // },
+      data: {
+        name: values.name,
+        emailId: values.email,
+        message: values.message,
+      },
+    };
+    try {
+      const response = await axios(config);
+      // const response=axios({
+      //     method: 'post',
+      //     url: url,
+      //     data: {
+      //       name: values.name,
+      //       emailId:values.email,
+      //       message:values.message
+      //     }});
+      console.log(response);
+      if (response.data === "Failed to receive mail") {
+        alert("Failed to receive message");
+      } else {
+        alert("Message receieved");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="p-4">
-                    <div  className="mx-5 space-y-4">
-                        <div>
-                            <input
-                                type="text"
-                                placeholder="Name"
-                                className={` w-3/4 sm:w-96 h-9 text-sm rounded-lg text-black bg-gray-200 ${errors.fullName &&
-                                           "focus:border-red-500 focus:ring-red-500 border-red-500 "}`}
-                                {...register("fullName",
-                                 { required:{
-                                    value:true,
-                                    message: 'Full name is required'},
+  return (
+    <div className="w-full bg-transparent p-6 text-white">
+      <h1 className="font-bold text-center text-2xl sm:text-3xl">
+        <span className="text-primary">Contact</span> Us
+      </h1>
 
-                                    minLength:{
-                                        value:3,
-                                        message:"Please enter your full name",
-                                    },
+      <form onSubmit={handleSubmit(onSubmitForm)}>
+        <div className="p-4">
+          <div className="mx-5 space-y-4">
+            <div>
+              <input
+                name="name"
+                type="text"
+                placeholder="Name"
+                className={` w-3/4 sm:w-96 h-9 text-sm rounded-lg text-black bg-gray-200 ${
+                  errors.fullName &&
+                  "focus:border-red-500 focus:ring-red-500 border-red-500 "
+                }`}
+                {...register("name", {
+                  required: {
+                    value: true,
+                    message: "Full name is required",
+                  },
 
-                                    maxLength:{
-                                        value:20,
-                                        message:"Maximum allowed characters is 20",
-                                    },
+                  minLength: {
+                    value: 3,
+                    message: "Please enter your full name",
+                  },
 
-                                    pattern:{
-                                        value:/[a-zA-Z]+/,
-                                        message:"Please enter only alphabets",
-                                    }
-                                })}
-                            />
-                            <div>
-                            {errors.fullName && <span className="text-red-500 text-xs">{errors.fullName.message}</span>}
-                            </div>
-                        </div>
-                        <div>
-                            <input
-                                type="email"
-                                placeholder="myid@gmail.com"
-                                className={`w-3/4 sm:w-96 h-9 text-sm rounded-lg text-black bg-gray-200 ${errors.fullName &&
-                                    "focus:border-red-500 focus:ring-red-500 border-red-500 "}`}
-                                {...register("email",
-                                 { required:{
-                                    value:true,
-                                    message: 'Email is required'},
+                  maxLength: {
+                    value: 20,
+                    message: "Maximum allowed characters is 20",
+                  },
 
-                                    pattern:{
-                                        value:/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/,
-                                        message:"Please enter a valid email",
-                                    }
-                                })}
-                            />
-                            <div>
-                            {errors.email && <span className="text-red-500 text-xs">{errors.email.message}</span>}
-                            </div>
-                        </div>
-                        <div>
-                            <textarea
-                                placeholder="Message"
-                                className={` w-3/4 sm:w-96 h-10 text-sm rounded-lg text-black bg-gray-200 ${errors.messages &&
-                                    "focus:border-red-500 focus:ring-red-500 border-red-500 "}`}
-                                {...register("messages",
-                                { required:{
-                                    value:true,
-                                    message: 'Message is required'},
+                  pattern: {
+                    value: /[a-zA-Z]+/,
+                    message: "Please enter only alphabets",
+                  },
+                })}
+              />
+              <div>
+                {errors.name && (
+                  <span className="text-red-500 text-xs">
+                    {errors.name.message}
+                  </span>
+                )}
+              </div>
+            </div>
+            <div>
+              <input
+                name="emailId"
+                type="email"
+                placeholder="myid@gmail.com"
+                className={`w-3/4 sm:w-96 h-9 text-sm rounded-lg text-black bg-gray-200 ${
+                  errors.fullName &&
+                  "focus:border-red-500 focus:ring-red-500 border-red-500 "
+                }`}
+                {...register("email", {
+                  required: {
+                    value: true,
+                    message: "Email is required",
+                  },
 
-                                    minLength:{
-                                        value:3,
-                                        message:"Please enter a valid message",
-                                    },
+                  pattern: {
+                    value: /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/,
+                    message: "Please enter a valid email",
+                  },
+                })}
+              />
+              <div>
+                {errors.email && (
+                  <span className="text-red-500 text-xs">
+                    {errors.email.message}
+                  </span>
+                )}
+              </div>
+            </div>
+            <div>
+              <textarea
+                // name="message"
+                placeholder="Message"
+                className={` w-3/4 sm:w-96 h-10 text-sm rounded-lg text-black bg-gray-200 ${
+                  errors.messages &&
+                  "focus:border-red-500 focus:ring-red-500 border-red-500 "
+                }`}
+                {...register("message", {
+                  required: {
+                    value: true,
+                    message: "Message is required",
+                  },
 
-                                    maxLength:{
-                                        value:200,
-                                        message:"Maximum allowed characters is 200",
-                                    },
-                                })}
-                            />
-                            <div>
-                            {errors.messages && <span className="text-red-500 text-xs">{errors.messages.message}</span>}
-                            </div>
-                        </div>
-                        <div>
-                            <input
-                                type="submit"
-                                value="Submit"
-                                className="bg-primary rounded-lg p-2 cursor-pointer hover:outline hover:bg-transparent"
-                            />
-                        </div>
-                    </div>
-                </div>
-            </form>
+                  minLength: {
+                    value: 3,
+                    message: "Please enter a valid message",
+                  },
+
+                  maxLength: {
+                    value: 200,
+                    message: "Maximum allowed characters is 200",
+                  },
+                })}
+              />
+              <div>
+                {errors.message && (
+                  <span className="text-red-500 text-xs">
+                    {errors.message.message}
+                  </span>
+                )}
+              </div>
+            </div>
+            <div>
+              <input
+                type="submit"
+                value="Submit"
+                className="bg-primary rounded-lg p-2 cursor-pointer hover:outline hover:bg-transparent"
+              />
+            </div>
+          </div>
         </div>
-    );
+      </form>
+    </div>
+  );
 };
 
 export default Contact;
