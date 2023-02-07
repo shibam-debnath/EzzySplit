8c0afeea5a15383541e96eb16633a3a72d200ae5
noob_coder <shibamdebnath07@gmail.com>
2023-02-07 20:19:27 +0530

    TopNav updated with notification and UserProfile

---
+++ src/data/NotificationData.jsx
@@ -0,0 +1,20 @@
+export const NotificationData = [
+  {
+    image: "./images/suraj.jpeg",
+    message: "Roman Joined the Team!",
+    desc: "Congratulate him",
+    time: "9:08 AM",
+  },
+  {
+    image: "./images/nikhil.png",
+    message: "New message received",
+    desc: "Salma sent you new message",
+    time: "11:56 AM",
+  },
+  {
+    image: "./images/rituraj.jpeg",
+    message: "New Payment received",
+    desc: "Check your earnings",
+    time: "4:39 AM",
+  },
+];
