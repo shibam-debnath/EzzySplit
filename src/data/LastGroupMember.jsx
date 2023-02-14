11b825303280aa66ac2941dd79fa4b4e0649db21
Mohit <mohitranjan2001r@gmail.com>
2023-02-14 23:20:47 +0530

    modify last group

---
+++ src/data/LastGroupMember.jsx
@@ -0,0 +1,39 @@
+export const LastGroupMember = [
+    {
+        id:"1",
+        name:"Suraj",
+        expenses:"1500",
+        created:"22/12/2022",
+      
+    },
+    {
+        id:"2",
+        name:"Shivham",
+        expenses:"1500",
+        created:"22/12/2022",
+    },
+    {
+        id:"3",
+        name:"Sxbujhd",
+        expenses:"1500",
+        created:"22/12/2022",
+    },
+    {
+        id:"4",
+    
+        name:"mfkbm",
+        expenses:"1500",
+        created:"22/12/2022",
+      
+    },
+    {
+        id:"5",
+        
+        name:"hfeh",
+        created:"22/12/2022",
+        expenses:"1500",
+    },
+    
+
+    
+];
